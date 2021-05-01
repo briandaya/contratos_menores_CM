cbe65db77d1aa3f4b423d00e39c465346307bd0e
Brianda <briandaya@gmail.com>
2021-05-01 04:44:41 +0200

    Add files via upload

--- js/counter.js
+++ js/counter.js
@@ -1,25 +1,26 @@
-
-	$.fn.jQuerySimpleCounter = function( options ) {
-	    var settings = $.extend({
-	        start:  0,
-	        end:    100,
-	        easing: 'swing',
-	        duration: 400,
-	        complete: ''
-	    }, options );
-
-	    var thisElement = $(this);
-
-	    $({count: settings.start}).animate({count: settings.end}, {
-			duration: settings.duration,
-			easing: settings.easing,
-			step: function() {
-				var mathCount = Math.ceil(this.count);
-				thisElement.text(mathCount);
-			},
-			complete: settings.complete
-		});
-	};
+function numberWithDots(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".");
+}
 
 
-$('#counter1').jQuerySimpleCounter({end: 2537206,duration: 4000});
+$.fn.jQuerySimpleCounter = function( options ) {
+    var settings = $.extend({
+        start:  0,
+        end:    100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options );
+    var thisElement = $(this);
+    $({count: settings.start}).animate({count: settings.end}, {
+		duration: settings.duration,
+		easing: settings.easing,
+		step: function() {
+			var mathCount = Math.ceil(this.count);
+			thisElement.text(numberWithDots(mathCount));
+		},
+		complete: settings.complete
+	});
+};
+
+$('#counter1').jQuerySimpleCounter({end: 2537206,duration: 5000});
